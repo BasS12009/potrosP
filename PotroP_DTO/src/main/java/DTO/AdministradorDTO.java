1c497bee34a3e32c5d42356fa27f74c4bc33cdaa
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-06-01 14:43:22 -0700

    Update AdministradorDTO.java

--- PotroP_DTO/src/main/java/DTO/AdministradorDTO.java
+++ PotroP_DTO/src/main/java/DTO/AdministradorDTO.java
@@ -9,5 +9,32 @@ package DTO;
  * @author diana
  */
 public class AdministradorDTO {
+    private String correo;
+    private String contrasena;
+
+    public AdministradorDTO() {
+    }
+
+    public AdministradorDTO(String correo, String contrasena) {
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    
     
 }
