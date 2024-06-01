684d522de971068d616e90d68d26d9c8678c4a8a
Gh0zTy1 <160164643+Gh0zTy1@users.noreply.github.com>
2024-06-01 13:39:56 -0700

    creacion de entidades para el caso de uso de prestamo de vehiculo (primera idea)

---
+++ PotroP_Persistencia/src/main/java/entidades/Vehiculo.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author caarl
+ */
+public class Vehiculo {
+    private String id;
+    private String marca;
+    private String modelo;
+    private int año;
+    private String placa;
+    private String tipo;
+    private Chofer chofer;
+    
+    // Constructores
+    public Vehiculo(String id, String marca, String modelo, int año, String placa, String tipo, Chofer chofer) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.placa = placa;
+        this.tipo = tipo;
+        this.chofer = chofer;
+    }
+
+    public Vehiculo() {
+    }
+    
+    
+    
+    // Getters y Setters
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+    
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+    public String getModelo() {
+        return modelo;
+    }
+    
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
+    public int getAño() {
+        return año;
+    }
+    
+    public void setAño(int año) {
+        this.año = año;
+    }
+    
+    public String getPlaca() {
+        return placa;
+    }
+    
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public Chofer getChofer() {
+        return chofer;
+    }
+    
+    public void setChofer(Chofer chofer) {
+        this.chofer = chofer;
+    }
+}
