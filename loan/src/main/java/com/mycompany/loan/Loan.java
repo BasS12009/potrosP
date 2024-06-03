a078a1e8de2bce6799a10b53007ddc7ca3ad231e
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-06-02 20:02:06 -0700

    subsistemas

---
+++ loan/src/main/java/com/mycompany/loan/Loan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loan;
+
+/**
+ *
+ * @author skevi
+ */
+public class Loan {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ loggin/src/main/java/autenticator/Loggin.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package autenticator;
+
+/**
+ *
+ * @author skevi
+ */
+public class Loggin {
+
+    
+}
