9194e9b173fd32c179bf78603036db1c91ecabc7
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-07-02 13:38:57 -0700

    c

--- PotroP_Persistencia/src/main/java/Pruebas/PrestamoDAO.java
+++ PotroP_Persistencia/src/main/java/Pruebas/PrestamoDAO.java
@@ -4,6 +4,7 @@
  */
 package Pruebas;
 
+
 /**
  *
  * @author diana
@@ -14,7 +15,41 @@ public class PrestamoDAO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//       // Conectar a la base de datos de MongoDB
+//        MongoClientURI uri = new MongoClientURI("mongodb://localhost:27017");
+//        MongoClient mongoClient = new MongoClient(uri);
+//        MongoDatabase database = mongoClient.getDatabase("PotroP");
+//        MongoCollection<Prestamo> prestamoCollection = database.getCollection("prestamos", Prestamo.class);
+//
+//        // Crear una instancia de PrestamoDAO
+//        PrestamoDAO prestamoDAO = new PrestamoDAO(prestamoCollection);
+//
+//        // Crear un objeto Prestamo
+//        Prestamo prestamo = new Prestamo("Compra de equipo", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 15), "ABC123", "empleado@empresa.com");
+//
+//        try {
+//            // Agregar el préstamo a la base de datos
+//            prestamoDAO.agregar(prestamo);
+//            System.out.println("Préstamo agregado exitosamente");
+//
+//            // Buscar el préstamo por su ID
+//            ObjectId id = new ObjectId(prestamo.getId());
+//            Prestamo prestamoRecuperado = prestamoDAO.buscarPorId(id);
+//
+//            // Verificar que el préstamo recuperado es igual al original
+//            if (prestamoRecuperado != null && prestamoRecuperado.equals(prestamo)) {
+//                System.out.println("Préstamo recuperado exitosamente: " + prestamoRecuperado);
+//            } else {
+//                System.out.println("Error al recuperar el préstamo");
+//            }
+//
+//        } catch (DAOException e) {
+//            System.err.println("Error en la operación con la base de datos: " + e.getMessage());
+//        } finally {
+//            // Cerrar la conexión
+//            mongoClient.close();
+//        }
+//    }
+    }}
     
-}
+
